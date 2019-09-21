1eeeb65e5163c6a0667b690d75025f649a480de5
Julia <julia1032003@gmail.com>
2019-09-21 16:25:24 +0800

    Initial Commit

---
+++ DrawSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DrawSwiftUI
+//
+//  Created by Julia Wang on 2019/9/21.
+//  Copyright © 2019 Julia Wang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
